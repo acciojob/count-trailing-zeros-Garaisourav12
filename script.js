2ca56afcb78b1ace004ef6e3ca4fbbc7479a5473
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-02-21 18:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	// Initialize count of trailing zeros
+    let count = 0;
+
+    // Keep dividing n by powers of 5 and update count
+    for (let i = 5; n / i >= 1; i *= 5) {
+        count += Math.floor(n / i);
+    }
+
+    return count;
 }
 
 const input = prompt("Enter a number");
